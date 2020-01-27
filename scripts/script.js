556cc2274cef5e5737ef5d117c4f43fba28f0fb8
Dariuszb94 <59793846+Dariuszb94@users.noreply.github.com>
2020-01-27 21:16:10 +0100

    Add files via upload
    second calc add

--- scripts/script.js
+++ scripts/script.js
@@ -7,12 +7,27 @@
   $(document).ready(function(){
 
 
-    $('#button').click(function pi (){
-    	var number=$("#piInput").val();
-$( "#pii" ).empty();
-  return  	$( "#pii" ).append(Math.PI.toFixed(number));
+    $('#buttonPi').click(function pi (){
+    	let number=$("#piInput").val();
+    	number=parseInt(number);
+if (number<15 && Number.isInteger(number))
+	{$( "#pii" ).empty();
+  return  	$( "#pii" ).append(Math.PI.toFixed(number));}
+  else 
+  	return alert("Number too large or not an integer");
+});
+
+ $('#buttonE').click(function e (){
+
+    	let numberE=$("#eInput").val();
+    	numberE=parseInt(numberE);
+if (numberE<15 && Number.isInteger(numberE))
+	{$( "#ee" ).empty();
+  return  	$( "#ee" ).append(Math.exp(1).toFixed(numberE));}
+  else 
+  	return alert("Number too large or not an integer");
  
 
-}
-);
+
+});
   });
